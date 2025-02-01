2335935f0ac191f4c5ed546e446ff018759d45d3
Suhira30 <suhirabalarajan@gmail.com>
2025-02-01 13:29:02 +0530

    countdown|todolist

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<!-- 1️⃣ Counter App
+A simple counter that allows increment, decrement, and reset.
+
+📝 Features
+Uses Svelte’s reactive state.
+Buttons to increase, decrease, and reset. -->
+
+<script>
+    import TodoList from "../routes/todolist/+page.svelte"
+    let count=$state(0)
+    function increment(){
+        count+=1
+    }
+    function decrement(){
+        count-=1
+    }
+    function reset(){
+        count=0
+    }
+</script>
+<main>
+<div class="flex flex-col items-center justify-center min-h-screen bg-orange-200 text-3xl">
+   <h1 class="text-gray-500 text-xl text-center mb-5">Count : </h1> 
+   {count}
+    <div class="flex flex-col md:flex-row gap-x-4  gap-y-4 mt-10">
+        <div>
+        <button onclick={increment} class=" bg-green-500 border-2 border-cyan-900 rounded w-28">
+            Plus
+        </button>
+        </div>
+        <div>
+        <button onclick={decrement} class=" bg-red-400 border-2 border-red-600 rounded w-28">
+            Minus
+        </button>
+        </div>
+        <div>
+        <button onclick={reset} class=" bg-yellow-300 border-2 border-yellow-600 rounded  w-28">
+            Reset
+        </button>
+        </div>
+    </div>
+</div>
+<div class="h-screen">
+    <TodoList/>
+</div>
+</main>
---
+++ src/routes/todolist/+page.svelte
@@ -0,0 +1,51 @@
+<!-- 2️⃣ To-Do List
+A basic to-do list with state management.
+
+📝 Features
+Add and remove tasks.
+Use Svelte’s reactivity to update UI. -->
+
+<script>
+	import { writable } from "svelte/store";
+    const tasks=writable( []);
+    if (typeof window !== 'undefined') {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks.set(storedTasks);
+    }
+
+
+    let newTask="";
+    function addTasks(){
+        if(newTask.trim() !== ""){
+            tasks.update(currentTasks =>{
+               const updatedTask= [...currentTasks, newTask];
+                localStorage.setItem('tasks',JSON.stringify(updatedTask));
+                return updatedTask;
+            });
+            newTask="";
+        }
+    }
+    function removeTask(index){
+        tasks.update(currentTask=>currentTask.filter((_,i)=>i!==index))
+    }
+</script>
+<main class="">
+    <div >
+        <h1 class="text-center font-semibold text-2xl justify-center items-center"> To-Do-List</h1>
+        <div class="flex flex-col items-center justify-center ">
+            <div class="flex flex-row gap-4">
+                <input type="text" placeholder="Type your Task" class="w-64" bind:value={newTask}>
+                <button onclick={addTasks} class="border-2  broder-black-300 bg-rose-400 rounded-xl w-20 hover:bg-purple-200 hover:border-black">Add!</button>
+            </div>
+            <div class="width-full">
+            {#each $tasks as task,index}
+                <div class="flex justify-between items-start">
+                    <span class="w-[280px] break-words mb-8 ">{task}</span>
+                    <button onclick={removeTask(index)} class="bg-red-500 w-20 border-2 mt-5 rounded-xl">Remove</button>
+                </div>
+            {/each}
+            </div>
+        </div>
+    </div>
+       
+</main>
